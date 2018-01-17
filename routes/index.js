4de3be1594e548cbdad8a10beb64a0c38790d989
udonka <icymasa@gmail.com>
2018-01-17 16:37:16 +0900

    moving to react

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/react', function(req, res, next) {
+  res.render('react', { title: 'Express' });
+});
+
 module.exports = router;
